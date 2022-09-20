25f39fef65216c9966dbbb60c47a3dde154dd12f
Jhonatan F. Ciriaco <jhonatanferreira13@hotmail.com>
2022-09-19 23:04:15 -0300

    [refact]: alteração de senha do mysql

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
 app.config['MYSQL_DATABASE_DB'] = 'zssn_db'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
