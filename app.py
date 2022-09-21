bdad0dd0f8db3286fcee240ce8614c4f4112f1e4
Jhonatan Ciriaco <jhonatanferreira13@hotmail.com>
2022-09-21 18:37:31 -0300

    [refactor] adicionado modo factory para execução do pytest

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from flaskext.mysql import MySQL
 
-app = Flask(__name__)
-CORS(app)
+mysql = MySQL()
+
+from routes import app_routes
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['MYSQL_DATABASE_USER'] = 'root'
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+    app.config['MYSQL_DATABASE_DB'] = 'zssn_db'
+    app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+    
+    app_routes(app)
+    mysql.init_app(app)
+
+    return app
+
